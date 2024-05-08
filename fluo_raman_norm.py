c465d9ae5e81a4f71fc2d9dd878d90ee0e37f6f5
enitsuj23 <160878237+enitsuj23@users.noreply.github.com>
2024-05-08 17:17:45 +0200

    Update fluo_raman_norm.py
    Fonctions concernant la liste excited wavelengths

--- fluo_raman_norm.py
+++ fluo_raman_norm.py
@@ -1,6 +1,89 @@
 import numpy as np    
 import pandas as pd
 
+#Initial condition
+a=input("Are your values in nanometers? If yes, write Yes, if not write No:")
+if a== "Yes" or a=="yes":
+    pass
+else:
+    raise ValueError("Please make sure your values are in nanometers otherwise the program doesn't work :)")
+    
+#step
+step=input("Please insert the measure step of your spectroscopy machine (no unit needed):")
+
+
+
+"""
+This fonction converts the coma in a point so that python recognises the number under the float form.
+
+Parameters: the step previously entered.
+
+Returns: the initial step if it is an integer or in the float form or the step converted in float.
+"""
+
+def conversion_coma_point(step):
+    if "," in step:
+        step = step.replace(",", ".")
+        return step
+    else:
+        return step
+
+step=conversion_coma_point(step)
+
+#Raise ValueError if step<0
+
+if float(step) < 0:
+    raise ValueError("The step must be positive.")
+
+
+#First and last excited wavelength values
+
+first=int(input("Please insert the first excited wavelength value(no unit needed):"))
+last=int(input("Please insert the last excited wavelength value(no unit needed):"))
+
+# Raise ValueError if first>last
+if first>last:
+    raise ValueError("Your first value should be smaller than your last.")
+
+
+
+"""
+This fonction creates a list with excited wavelengths in terms of the step, from the first value and last 
+entered value. It consists of two main parts: the first if the step is an integer, the other if the step ia a float.
+
+Parameters: first(int) and last (int) excited wavelengths value.
+
+Returns: a list of excited wavelength value in the exact where the integer values are in the form int, and the 
+others are in the form float.
+"""
+
+def excited_wavelength_list(first, last, step):
+    # Conversion first, last, end in float
+    first = float(first)
+    last = float(last)
+    step = float(step)
+    
+    l_x = []
+    
+
+    if isinstance(step, int):
+        # If the step is an integer
+        l_x = list(range(int(first), int(last) + 1, int(step)))
+    else:
+        # If the step is a float
+        current = first
+        while current <= last:
+            rounded_current = round(current, 1)# Round the values with one decimal 
+            if rounded_current.is_integer():
+                l_x.append(int(rounded_current))# If the wavelengths is an integer,remove the coma  
+            else:
+                l_x.append(rounded_current)
+            current += step
+    
+    return l_x
+
+
+
 def Area(files_directory):
     '''
     Calculate the Area of the water Raman peak for each exitation wavelenght.
