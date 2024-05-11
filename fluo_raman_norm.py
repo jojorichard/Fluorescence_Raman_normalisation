61aa56e184ddb21b5b329488fc2c821dbeebbf27
eilaroc32 <160879372+eilaroc32@users.noreply.github.com>
2024-05-11 18:55:14 +0200

    Update for the graphic part

--- fluo_raman_norm.py
+++ fluo_raman_norm.py
@@ -1,5 +1,7 @@
 import numpy as np    
 import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objs as go
 
 #Initial condition
 a=input("Are your values in nanometers? If yes, write Yes, if not write No:")
@@ -131,3 +133,293 @@ def Raman_normalisation(files_directory, Area):
 def fluo_raman_norm(files_directory):
     A = Area(files_directory)
     Raman_normalisation(files_directory, A)
+
+
+
+
+
+def plot_fluorescence_graph():
+    """
+    Function to plot fluorescence data on a standard graph and/or an interactive graph.
+
+    This function prompts the user to provide the file location, the sheet name, 
+    the names of the excitation and emission wavelength columns in an Excel file, 
+    and the wavelength of interest in nanometers (nm). It then plots the fluorescence 
+    data on a standard graph and an interactive graph using Plotly.
+
+    Returns:
+    None
+    """
+    # Wavelenth of interest
+    chemin = input("What is the exact file location : ")
+    df = pd.read_excel(chemin, sheet_name=None)
+    Feuil = input("What is the exact name of the sheet on Excel  : ")
+    X = input("What is the exact name of the excitation wavelength column on Excel : ")
+    wavelength = input("What is the wavelength of interest in nm : ")
+    Y = input("What is the exact name of the emission wavelength column of interest on Excel : ")
+
+    EmWl = df[Feuil][X]
+    Int = df[Feuil][Y]
+
+    # Ask the user if he wants to display the standard graph
+    while True:
+        choice1 = input("Would you like to plot a standard graph ? (yes/no) : ").lower()
+        if choice1 == "yes":
+            # Size of the graph
+            plt.figure(figsize=(40,30))
+
+            # Plotting of the point cloud
+            plt.plot(EmWl, Int, label=f'λ = {wavelength}', color='black')
+            plt.legend(fontsize=30)
+
+            # Formating of the axis and title of the graph
+            plt.title("Raman spectrum", fontsize=70)
+            plt.xlabel(r'Excitation wavelength [nm]', fontsize=50)
+            plt.ylabel(r'Fluorescence', fontsize=50)
+
+            # Graduation of the axis
+            plt.xticks(fontsize=30)
+            plt.yticks(fontsize=30)
+            plt.show()
+            plt.savefig('Raman_spectrum.svg')
+            break
+        elif choice1 == "no":
+            break
+        elif choice1 != "no":
+            print("Please answer by 'yes' or 'no'.")
+
+    # Creation of an interactive graphic with zoom
+    fig = go.Figure()
+
+    # Adding of a scatter plot to the interactive graphic
+    fig.add_trace(go.Scatter(x=EmWl, y=Int, marker=dict(color='black'), mode='lines', name='Data'))
+
+    # Configuration of the layout of the interactive graphic
+    fig.update_layout(
+        title='Interactive graphics with zoom',
+        xaxis_title='Fluorescence', 
+        yaxis_title='Excitation wavelength [nm]',
+        title_font=dict(size=25),  # Size of the title 
+        title_x=0.5,  # To center the title
+        xaxis=dict(
+            title='Fluorescence',
+            title_font=dict(size=20),  # Size of the label of the x axis
+            rangeselector=dict(
+                buttons=list([
+                    dict(step="all") ])),
+            rangeslider=dict(visible=True), 
+            type="linear"),
+        yaxis=dict(title='Excitation wavelength [nm]', 
+                   title_font=dict(size=20)),  # Size of the label of the y axis
+        hovermode='closest',
+        height=700,  # Height in pixels
+        width=900 )   # Width in pixels
+
+    # Ask the user if he wants to display the interactive graph
+    while True:
+        choice2 = input("Would you like to plot an interactive graph ? (yes/no) : ").lower()
+        if choice2 == "yes":
+            fig.show()
+            break
+        elif choice2 == "no":
+            break
+        elif choice2 != "no":
+            print("Please answer by 'yes' or 'no'.")
+            
+
+
+def plot_superimposed_graphs():
+    """
+    Function to plot a superposition of graphs on a standard graph and, if wanted, an interactive graph.
+
+    This function prompts the user to input the number of graphs they want to superimpose. 
+    It then collects data for each graph, including file location, sheet name, column names 
+    for excitation and emission wavelengths, and the wavelength of interest. It plots both 
+    a static and an interactive graph showing the superimposed curves.
+
+    Returns:
+    None
+    """
+
+    while True:
+        try:
+            number_of_graph = int(input("How many graphs would you like to superimpose :"))
+            if number_of_graph > 7:
+                print("A maximum of 7 different graphs can be superimposed !")
+            elif number_of_graph < 2:
+                print("The number of graph should be at least 2 !")
+            else:
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+
+            
+    # Extraction of the values of interest
+
+    
+    # Creation of lists to store the values
+    chemins = []
+    Feuils = []
+    Xs = []
+    wavelengths = []
+    Ys = []
+
+    # Loop to collect variables for each graph
+    while True:
+        q1 = input("Are all the values in the same file ? (yes/no) ")
+        if q1 == "yes":
+            chemin = input("What is the exact file location : ")
+            for i in range(1, int(number_of_graph) + 1):
+                Feuil = input(f"What is exact name of the sheet on Excel for curve {i}: ")
+                X = input(f"What is the exact name of the excitation wavelength column on Excel for curve {i}: ")
+                wavelength = input(f"What is the wavelength of interest for curve {i} in nm: ")
+                Y = input(f"What is the exact name of the emission wavelength column of interest on Excel for curve {i}: ")
+                # Adding the variables to the corresponding list
+                chemins.append(chemin)
+                Feuils.append(Feuil)
+                Xs.append(X)
+                wavelengths.append(wavelength)
+                Ys.append(Y)
+            break
+        elif q1 == "no":
+            for i in range(1, int(number_of_graph) + 1):
+                chemin = input(f"What is the exact file location for curve {i}: ")
+                Feuil = input(f"What is exact name of the sheet on Excel for curve {i}: ")
+                X = input(f"What is the exact name of the excitation wavelength column on Excel for curve {i}: ")
+                wavelength = input(f"What is the wavelength of interest for curve {i} in nm: ")
+                Y = input(f"What is the exact name of the emission wavelength column of interest on Excel for curve {i}: ")
+                # Adding the variables to the corresponding list
+                chemins.append(chemin)
+                Feuils.append(Feuil)
+                Xs.append(X)
+                wavelengths.append(wavelength)
+                Ys.append(Y)
+            break
+        elif q1 != "no":
+            print("Please answer by 'yes' or 'no'.")
+    
+            
+    # General shape of the graph
+
+    
+    # Size of the graph, formating of the axis and title of the graph
+    plt.figure(figsize=(40,30))
+    plt.title("Raman spectrum", fontsize=70)
+    plt.xlabel(r'Excitation wavelength [nm]',  fontsize=50)
+    plt.ylabel(r'Fluorescence', fontsize=50)
+
+    # Graduation of the axis
+    plt.xticks(fontsize=30)
+    plt.yticks(fontsize=30)
+
+    
+    # Plot of the superimposed point cloud 
+
+
+    colors = ["black", "blue", "red", "green", "magenta", "yellow", "cyan"]
+    for i in range(number_of_graph):
+        df = pd.read_excel(chemins[i], sheet_name=None)
+        EmWl = df[Feuils[i]][Xs[i]]
+        Int = df[Feuils[i]][Ys[i]]
+        color = colors[i]
+        wavelength = wavelengths[i]
+        plt.plot(EmWl, Int, label = f'λ = {wavelength}',color = color)
+        plt.legend(fontsize = 40)
+
+        
+    # Plot of the interactive graph
+
+    # Creation of an interactive graphic with zoom
+    fig = go.Figure() 
+
+    # Add each curve from data in Excel files
+    for i, chemins in enumerate(chemins):
+        # Load the value from an Excel file
+        df = pd.read_excel(chemins) 
+
+        # Select columns corresponding to x and y values
+        x_col_name = Xs[i]
+        y_col_name = Ys[i]  
+        x_data = df[x_col_name]
+        y_data = df[y_col_name]
+    
+        # Add curve to figure
+        color = colors[i]
+        wavelength = wavelengths[i]
+        fig.add_trace(go.Scatter(x=x_data, y=y_data, marker=dict(color= color), mode='lines', name=f'λ = {wavelength}'))
+
+    # Configuration des boutons pour afficher/supprimer les courbes
+    buttons = []
+
+    # Add a button for each curve
+    for i in range(len(Xs)):
+        wavelength = wavelengths[i]
+        button = dict(
+        label=f'λ = {wavelength}',
+        method="update",
+        args=[{"visible": [True if j == i else False for j in range(len(Xs))]}, {"title": f'λ = {wavelength}'}])
+        buttons.append(button)
+
+    # Add a button to display all curves
+    button_all = dict(
+        label="All",
+        method="update",
+        args=[{"visible": [True for _ in range(len(Xs))]}, {"title": "All"}])
+    buttons.append(button_all)
+
+    # Add a button to hide all curves
+    button_none = dict(
+        label="None",
+        method="update",
+        args=[{"visible": [False for _ in range(len(Xs))]}, {"title": "None"}])
+    buttons.append(button_none)
+
+    # Configuration of the layout of the interactive graphic
+    fig.update_layout(
+        updatemenus=[
+            dict(
+                type="buttons",
+                buttons=buttons,
+                direction="left",
+                pad={"r": 10, "t": 10},
+                showactive=True,
+                x=0.02,
+                xanchor="left",
+                y=1.1,
+                yanchor="top",
+                font=dict(size=14))], # Font size
+        title='Interactive graphics with zoom',
+        xaxis_title='Fluorescence', 
+        yaxis_title='Excitation wavelength [nm]',
+        title_font=dict(size=25),  # Size of the title 
+        title_x=0.5,  # To center the title
+        xaxis=dict(
+            title='Fluorescence',
+            title_font=dict(size=20),  # Size of the label of the x axis
+            type="linear"),
+        yaxis=dict(title='Excitation wavelength [nm]', 
+                   title_font=dict(size=20)),  # Size of the label of the y axis
+        hovermode='closest',
+        height=650,  # Height in pixels
+        width=850 )   # Width in pixels
+    
+    # Ask the user if he wants to display the interactive graph
+    while True:
+        choice2 = input("Would you like to plot an interactive graph ? (yes/no) : ").lower()
+        if choice2 == "yes":
+            fig.show()
+            break
+        elif choice2 == "no":
+            break
+        elif choice2 != "no":
+            print("Please answer by 'yes' or 'no'.")
+
+            
+    # Displaying and saving the graph
+
+    
+    plt.savefig('Raman_spectrum.svg')  
+    plt.show()
+
+
+
