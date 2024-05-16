5cc95469f30f5f69782ffbe0ad9325f26486bdbd
jojorichard <160777950+jojorichard@users.noreply.github.com>
2024-05-16 16:48:54 +0200

    Addition of the possibility to remove Rayleigh scattering and to plot 3D graph

--- fluo_raman_norm.py
+++ fluo_raman_norm.py
@@ -144,53 +144,66 @@ def read_excel():
 
 
 
-def Area(files_directory):
+def Area(eem):
     '''
-    Calculate the Area of the water Raman peak for each exitation wavelenght.
+    Calculate the Area of the water Raman peak computed for 350 nm exitation waveleght and emission from 371 nm to 428 nm according to the following paper:
+    Lawaetz, A. J., & Stedmon, C. A. (2009). Fluorescence Intensity Calibration Using the Raman Scatter Peak of Water. Applied Spectroscopy, 63(8), 936-940.
+    https://journals.sagepub.com/doi/10.1366/000370209788964548
 
     Args: 
-    - files_directory the files directory of the spectroscopy matrice
+        - eem: dataframe containing the eem matrice created with de read_eem function
 
-    Returns: Arp the Area of the water Raman peak calculated using the trap
+    Returns: Arp the Area of the water Raman peak calculated using the trapezoidal rule
     '''
-    df = pd.read_excel(files_directory)
-    raman = df.loc[(df['EmWl [nm]'] >= 371) & (df['EmWl [nm]'] <= 428)]
+    raman = eem.loc[(eem['EmWl [nm]'] >= 371) & (eem['EmWl [nm]'] <= 428)]
     dif = np.diff(raman['EmWl [nm]'])
-    l_ex = (excited_wavelength_list(250,600,10))  # Ajouter partie qui demande les bornes et le pas
-    collumn = {}
-    Arp = pd.DataFrame(columns=[f'Int({i})' for i in l_ex], index=[0])
-    print(df)
-    print(Arp)
+    fluo_avg = (raman[350][:-1] + raman[350][1:]) / 2
+    A = np.sum(dif[0] * fluo_avg)
+    return A
 
-    for i in l_ex:
-        fluo_avg = (raman["Int(" + str(i) + ")"][:-1] + raman["Int(" + str(i) + ")"][1:]) / 2
-        A = np.sum(dif[0] * fluo_avg)
-        print(f'The Area of the water Raman peak at {i} nm is: {A}')
-        Arp[f'Int({i})'] = A
-    return Arp
-
-def Raman_normalisation(files_directory, Area):
+def Raman_normalisation(eem, Area):
     '''
-    Normalise all the fluorecence values in each exitation waveleght with the area of the Raman peak computed in the Area function
+    Normalise all the fluorecence values in each exitation waveleght with the area of the Raman peak computed in the Area function and create a new files with the normalised values
     
-    Args: files_directory the files directory of the spectroscopy matrice, Area a dataframe containing the wavelenght exitation in each collumn and the associaced Raman Area
+    Args:  
+    - eem: dataframe containing the eem matrice created with de read_eem function
+    - Area: a dataframe containing the wavelenght exitation in each collumn and the associaced Raman Area
     
-    Returns: A new files containing the normalised matrice
+    Returns: A dataframe containing the normalised matrice
     '''
-
-    df = pd.read_excel(files_directory)
-    columns_of_interest = df.columns[df.columns.str.startswith('Int')]  
-    normalised = df.copy()
-    normalised[columns_of_interest] = normalised[columns_of_interest].div(Area.iloc[0])
+ 
+    columns_of_interest = eem.columns[1:] 
+    normalised = eem.copy()
+    normalised[columns_of_interest] = eem[columns_of_interest].div(Area)
     normalised.to_excel('normalised_Raman.xlsx', index=False)
-    return normalised    
+    return normalised
 
 
 
+def fluo_raman_norm(eem):
+    A = Area(eem)
+    return Raman_normalisation(eem, A)
 
-def fluo_raman_norm(files_directory):
-    A = Area(files_directory)
-    Raman_normalisation(files_directory, A)
+
+def remove_rayleigh_scattering(eem, order=1, width=10):
+    '''
+    Allow to remove the first and second order Rayleigh scattering. It is recommanded to use this function before plotting the graph in order to have a better visualisation.
+    The Rayleigh scatteing are removed according the following paper:	Anal. Methods, 2013,5, 6557-6566, https://pubs.rsc.org/en/content/articlelanding/2013/ay/c3ay41160e
+
+    Args:
+        - eem: dataframe containing the eem matrice created with de read_eem function
+        - order: The order of the Rayleigh scattering that need to be remover. Allow the values 1 or 2. Default is set to 1
+        - width: The width of the cut. A higher cut could supress data of interest. Default set to 10 nm
+
+    Return: A copy of the eem DataFrame with the removed value set to NaN
+    '''
+    df = eem.copy()
+    if order not in [1, 2]:
+         raise ValueError("The value of 'order' must be either 1 or 2. This function removes Rayleigh scattering peaks of either first or second order.")
+    for col in df.columns[1:]:
+        df.loc[(df['EmWl [nm]'] >= order * float(col) - width) & (df['EmWl [nm]'] <= order * float(col) + width), col] = np.nan
+
+    return df
 
 
 
@@ -474,5 +487,96 @@ def plot_superimposed_graphs():
     plt.savefig('Raman_spectrum.svg')  
     plt.show()
 
+def plot_3D_contour_inter(eem, levels = 30, Normalisation = True):
+    '''
+    Plot an interactive 3D contour graph with the folowing axes:
+        - x: the emmited wavelenght in nm
+        - y: the exitation wavelenght in nm
+        - z: the intensity in a.u or R.u depending if the data are normalised or not
+    Args: 
+        - eem: dataframe containing the eem matrice created with de read_eem function
+        - levels: the number of levels showed on the graph. Higher values increase precision but can lead to a loss of legibility in certain areas where the intensity varies rapidly. Default value is set to 30
+        - Normalisation: Boolean used to specify whether or not normalisation has already been carried out. Only used to change the colorbar unit between a.u and R.u.
 
+    '''
+    if Normalisation:
+        unit = 'R.u'
+    else:
+        unit = 'a.u'
+    x = np.array(eem['EmWl [nm]'])
+    Z = eem.iloc[:, 1:].values
+    y = np.array(eem.columns[1:])
+
+    fig = go.Figure(data=go.Contour(x=x, y=y, z=Z.transpose(), colorscale='jet',ncontours=levels,))
+    fig.update_layout(
+    xaxis_title='λ<sub>em</sub> [nm]',
+    yaxis_title='λ<sub>ex</sub> [nm]', 
+    )
+    fig.update_traces(line_width=0.5)
+    fig.data[0].colorbar.title = f"Fl [{unit}]"
+    
+    fig.show()
+
+def plot_3D_contour(eem, levels = 25, Normalisation = True):
+    '''
+    Plot an 3D contour graph with the folowing axes:
+        - x: the emmited wavelenght in nm
+        - y: the exitation wavelenght in nm
+        - z: the intensity in a.u or R.u depending if the data are normalised or not
+    Args: 
+        - eem: dataframe containing the eem matrice created with de read_eem function
+        - levels: the number of levels showed on the graph. Higher values increase precision but can lead to a loss of legibility in certain areas where the intensity varies rapidly. Default value is set to 25
+        - Normalisation: Boolean used to specify whether or not normalisation has already been carried out. Only used to change the colorbar unit between a.u and R.u.
+
+    '''
+    if Normalisation:
+        unit = 'R.u'
+    else:
+        unit = 'a.u'
+   
+    x = np.array(eem['EmWl [nm]'])
+    Z = eem.iloc[:, 1:].values
+    y = np.array(eem.columns[1:])
+    X,Y = np.meshgrid(x,y)
+    Z_transposed = np.transpose(Z)
+    
+    plt.contourf(X,Y,Z_transposed, cmap='jet', levels = levels) 
+    plt.colorbar(label=f'Fl [{unit}]')
+
+    plt.contour(X, Y, Z_transposed, colors='k',linewidths=0.5, levels = levels)
+    plt.xlabel(r'$\lambda{em}$ [nm]')
+    plt.ylabel(r'$\lambda{ex}$ [nm]')
+
+
+    plt.show()  
+
+def plot_3D_surface_inter(eem, Normalisation = True):
+    '''
+    Plot an 3D surface interactive graph with the folowing axes:
+        - x: the emmited wavelenght in nm
+        - y: the exitation wavelenght in nm
+        - z: the intensity in a.u or R.u depending if the data are normalised or not
+    Args: 
+        - eem: dataframe containing the eem matrice created with de read_eem function
+        - Normalisation: Boolean used to specify whether or not normalisation has already been carried out. Only used to change the colorbar unit between a.u and R.u.
+    '''
+    
+    if Normalisation:
+        unit = 'R.u'
+    else:
+        unit = 'a.u'
+    x = np.array(eem['EmWl [nm]'])
+    Z = eem.iloc[:, 1:].values
+    y = np.array(eem.columns[1:])
+
+    fig = go.Figure(data=go.Surface(x=x, y=y, z=Z.transpose(), colorscale='jet'))
+    fig.update_layout(
+        scene=dict(
+            xaxis_title='λ<sub>em</sub> [nm]',
+            yaxis_title='λ<sub>ex</sub> [nm]',
+            zaxis_title=f'Fl [{unit}]'
+        )
+    )
+    
+    fig.show()
 
