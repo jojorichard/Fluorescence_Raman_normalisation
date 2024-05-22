8e41d77511f381fbc0609d318ceb0d2a51a723d5
jojorichard <160777950+jojorichard@users.noreply.github.com>
2024-05-22 18:16:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,21 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        return [line.strip() for line in lines if line.strip() and not line.startswith('#')]
+
 setup(
-    name='EEM',
-    version='0.1',
+    name='EEM',  
+    version='0.1',  
     packages=find_packages(),
-    install_requires=[
-        'contourpy',
-        'cycler==0.12.1',
-        'exceptiongroup==1.2.1',
-        'fonttools==4.51.0',
-        'iniconfig==2.0.0',
-        'kiwisolver==1.4.5',
-        'matplotlib',
-        'mypy==1.10.0',
-        'mypy-extensions==1.0.0',
-        'numpy',
-        'packaging==24.0',
-        'pandas',
-        'pillow==10.3.0',
-        'plotly==5.22.0',
-        'pluggy==1.5.0',
-        'pyparsing==3.1.2',
-        'pytest==8.2.0',
-        'python-dateutil==2.9.0.post0',
-        'pytz==2024.1',
-        'six==1.16.0',
-        'tenacity==8.3.0',
-        'tk==0.1.0',
-        'tomli==2.0.1',
-        'typing-extensions==4.11.0',
-        'tzdata==2024.1'
-    ],
-    dependency_links=[
-        'git+https://github.com/jojorichard/Fluorescence_Raman_normalisation.git'
-    ]
+    install_requires=parse_requirements('requirements.txt'),
+    url='https://github.com/jojorichard/EEM',  
+    author='Premier Auteur', 
+    author_email='premier.auteur@example.com',  
+    maintainer='Premier Auteur, Deuxième Auteur, Jonas Richard',  
+    maintainer_email='premier.auteur@example.com, deuxieme.auteur@example.com, jonas.richard@hotmail.fr', 
+    description='Un package pour l’analyse des données de fluorescence',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown', 
 )
