1a5ddb460d6c80b77459c343c1c651d50a3d9595
jojorichard <160777950+jojorichard@users.noreply.github.com>
2024-05-23 15:02:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,14 @@ setup(
     version='0.1',  
     packages=find_packages(),
     install_requires=parse_requirements('requirements.txt'),
+     dependency_links=[
+        'https://github.com/jojorichard/Fluorescence_Raman_normalisation.git#egg=Fluorescence_Raman_normalisation'
+    ],
     url='https://github.com/jojorichard/EEM',  
     author='Jonas Richard', 
     author_email='jonas.richard@hotmail.fr',  
     maintainer='Justine Serra, Coralie Reuse, Jonas Richard',  
-    maintainer_email='justine.serratosio@gmail.com, coralie.resu23@gmail.com, jonas.richard@hotmail.fr', 
+    maintainer_email='justine.serratosio@gmail.com, coralie.reuse23@gmail.com, jonas.richard@hotmail.fr', 
     description='Un package pour l’analyse des données de fluorescence',  
     long_description=open('README.md').read(),  
     long_description_content_type='text/markdown', 
