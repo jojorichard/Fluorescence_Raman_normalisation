90b444e11abd6066de5bd47ff6152d6b1c85b638
jojorichard <160777950+jojorichard@users.noreply.github.com>
2024-05-23 22:09:29 +0200

    Update __init__.py

--- package/__init__.py
+++ package/__init__.py
@@ -1,1 +1,12 @@
 __name__ = "EEM"
+from .EEM import read_eem
+from .EEM import read_excel
+from .EEM import Area
+from .EEM import Raman_normalisation
+from .EEM import fluo_raman_norm
+from .EEM import remove_rayleigh_scattering
+from .EEM import plot_fluorescence_graph
+from .EEM import plot_superimposed_graphs
+from .EEM import plot_3D_contour_inter
+from .EEM import plot_3D_contour
+from .EEM import plot_3D_surface_inter
